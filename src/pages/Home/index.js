87240f7cfd54a567de2fc293a8a12ebd6b7b33b6
João Victor Corado <jhonisilva545@gmail.com>
2023-03-09 20:52:50 -0300

    novo
    Nove layout do site

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import { Button, Col, Row } from 'react-bootstrap';
 import { Header } from "../../componets/Header"
 import QuemSomos from '../../assets/image/sobrepets.webp'
+import Custom from '../../assets/image/dogpets2.webp'
 import { useNavigate } from 'react-router-dom';
 
 
@@ -10,6 +11,10 @@ export const Home=()=>{
 
     function redirectSobre (){
         navigate('/conheca')
+    }
+
+    function redirectProdutos (){
+        navigate('/produtos')
     }
 
     return (
@@ -31,37 +36,42 @@ export const Home=()=>{
                 </div>
             </section>
             <section className='w-100 vh-50 d-flex bg-light align-items-center justify-content-center p-5 gap-5'>
-                <img src="https://petsmellon.com.br/uploads/images/2022/06/5-partes-naturais-1655323718.png" alt="" width={250}/>
+                <img src="https://petsmellon.com.br/uploads/images/2022/06/1-petiscos-1655323403.png" alt="" width={250}/>
                 <div className='w-50 d-flex flex-column gap-3'>
                     <h1 style={{color:'#828282',fontSize:'4rem'}}>PETISCOS</h1>
                     <p className='text-start lh-lg fs-5'> Os snacks são identificados como alimentos específicos. São produtos compostos por ingredientes, matérias-primas ou aditivos destinados exclusivamente à alimentação de animais de companhia com finalidade de agrado, prêmio ou recompensa.</p>
-                    <button className='btn p-3 text-white w-50 fs-5' style={{backgroundColor:'#FF9200'}}>VER SOBRE O PRODUTO</button>
+                    <button onClick={() => redirectProdutos()} className='btn p-3 text-white w-75 fs-5' style={{backgroundColor:'#FF9200'}}>SAIBA MAIS SOBRE NOSSOS PRODUTOS</button>
                 </div>
             </section>
-            <section className='W-100 vh-50 d-flex flex-column p-5  gap-5 '>
-                <h1 style={{color:'#828282',fontSize:'3rem'}}>CUSTOMIZAÇÕES</h1>
-                <Row className='d-flex '>
-                    <Col lg='4' className='d-flex align-items-center fs-3 gap-3'>
-                        <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha Econômica
-                    </Col>
-                    <Col lg='4' className='d-flex align-items-center fs-3 gap-3'>
-                        <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha High Premium
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg='4'  className='d-flex align-items-center fs-3 gap-3'>
-                        <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt=""/>Linha Mainstream
-                    </Col>
-                    <Col lg='4' className='d-flex align-items-center fs-3 gap-3'>
-                        <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha Super Premium
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg='4' className='d-flex align-items-center fs-3 gap-3'>
-                        <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha Premium
-                    </Col>
-                </Row>
-                <button className='btn p-3 text-white w-50 fs-5' style={{backgroundColor:'#FF9200'}}>SAIBA MAIS SOBRE NOSSSAS FÓRMULAS</button>
+            <section className='W-100 vh-50 d-flex  p-5  gap-5 '>
+                <div className='d-flex flex-column gap-5 w-75'>
+                    <h1 style={{color:'#828282',fontSize:'3rem'}}>CUSTOMIZAÇÕES</h1>
+                    <Row className='d-flex '>
+                        <Col lg='6' className='d-flex align-items-center fs-3 gap-3'>
+                            <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha Econômica
+                        </Col>
+                        <Col lg='6' className='d-flex align-items-center fs-3 gap-3'>
+                            <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha High Premium
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg='6'  className='d-flex align-items-center fs-3 gap-3'>
+                            <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt=""/>Linha Mainstream
+                        </Col>
+                        <Col lg='6' className='d-flex align-items-center fs-3 gap-3'>
+                            <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha Super Premium
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg='6' className='d-flex align-items-center fs-3 gap-3'>
+                            <img src="https://petsmellon.com.br/themes/pets/images/img-patinha-linhaatuacao.png" alt="" />Linha Premium
+                        </Col>
+                    </Row>
+                    <button className='btn p-3 text-white w-75 fs-5' style={{backgroundColor:'#FF9200'}}>SAIBA MAIS SOBRE NOSSSAS FÓRMULAS</button>
+                </div>
+                <div className='w-50 d-flex align-items-center '>
+                    <img src={Custom} alt="" style={{width:'100%'}} className="ms-4"/>
+                </div>
             </section>
         </div>
     );
--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -8,10 +8,34 @@ export const Produtos=()=>{
         <div className=''>
             <Header/>
             <section className='w-100 vh-50 d-flex bg-light align-items-center justify-content-center p-5 gap-5'>
-                <img src="https://petsmellon.com.br/uploads/images/2022/06/5-partes-naturais-1655323718.png" alt="" width={250}/>
+                <img src="https://petsmellon.com.br/uploads/images/2022/06/1-petiscos-1655323403.png" alt="" width={250}/>
                 <div className='w-50 d-flex flex-column gap-3'>
                     <h1 style={{color:'#828282',fontSize:'4rem'}}>PETISCOS</h1>
                     <p className='text-start lh-lg fs-5'> Os snacks são identificados como alimentos específicos. São produtos compostos por ingredientes, matérias-primas ou aditivos destinados exclusivamente à alimentação de animais de companhia com finalidade de agrado, prêmio ou recompensa.</p>
+                    <button className='btn p-3 text-white w-50 fs-5' style={{backgroundColor:'#FF9200'}}>VER SOBRE O PRODUTO</button>
+                </div>
+            </section>
+            <section className='w-100 vh-50 d-flex flex-row-reverse  align-items-center justify-content-center p-5 gap-5'>
+                <img src="https://petsmellon.com.br/uploads/images/2022/06/6-stand-up-pouch-1655323377.png" alt="" width={250}/>
+                <div className='w-50 d-flex flex-column gap-3'>
+                    <h1 style={{color:'#828282',fontSize:'4rem'}}>STAND UP POUCH</h1>
+                    <p className='text-start lh-lg fs-5'>São classificados como alimentos completos, os enlatados e bolsas geralmente se diferenciam apenas na embalagem apresentada.</p>
+                    <button className='btn p-3 text-white w-50 fs-5' style={{backgroundColor:'#FF9200'}}>VER SOBRE O PRODUTO</button>
+                </div>
+            </section>
+            <section className='w-100 vh-50 d-flex flex-row-reverse bg-light align-items-center justify-content-center p-5 gap-5'>
+                <img src="https://petsmellon.com.br/uploads/images/2022/06/3-enlatados-1655323388.png" alt="" width={250}/>
+                <div className='w-50 d-flex flex-column gap-3'>
+                    <h1 style={{color:'#828282',fontSize:'4rem'}}>ENLATADOS</h1>
+                    <p className='text-start lh-lg fs-5'> São classificados como alimentos completos, os enlatados e bolsas geralmente se diferenciam apenas na embalagem apresentada.</p>
+                    <button className='btn p-3 text-white w-50 fs-5' style={{backgroundColor:'#FF9200'}}>VER SOBRE O PRODUTO</button>
+                </div>
+            </section>
+            <section className='w-100 vh-50 d-flex flex-row-reversebg-light align-items-center justify-content-center p-5 gap-5'>
+                <img src="https://petsmellon.com.br/uploads/images/2022/06/5-partes-naturais-1655323718.png" alt="" width={250}/>
+                <div className='w-50 d-flex flex-column gap-3'>
+                    <h1 style={{color:'#828282',fontSize:'4rem'}}>PARTES NATURAIS</h1>
+                    <p className='text-start lh-lg fs-5'> Classificados como produtos mastigáveis, os Body Parts são produtos à base de subprodutos de origem animal, podendo abarcar ingredientes de origem vegetal.</p>
                     <button className='btn p-3 text-white w-50 fs-5' style={{backgroundColor:'#FF9200'}}>VER SOBRE O PRODUTO</button>
                 </div>
             </section>
